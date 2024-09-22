484d10e3b09d234ebc13e89232a685f21643a74d
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-22 23:43:48 +0545

    project-swapping

---
+++ main.py
@@ -0,0 +1,12 @@
+x=input("Enter the value of x:")
+y=input("Enter the value of y:")
+z=input("Enter the value of z:")
+
+temp=x
+x=y
+y=z
+z=temp
+
+print("Value of x after swapping:",x)
+print("Value of y after swapping:",y)
+print("Value of z after swapping:",z)
